f2339cb6c795f38d2b48f71e39c8edb5f3006540
nasirbashak <nasirbashak007@gmail.com>
2018-07-16 12:29:12 +0530

    Stem color optional argument and variable values collosion solved

--- app/src/main/java/com/nasirbashak007gmail/colorchange/MainActivity.java
+++ app/src/main/java/com/nasirbashak007gmail/colorchange/MainActivity.java
@@ -12,13 +12,15 @@ import e.nasirbashak.backgroungcolorchange.ChangeColor;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView myTextView;
+    TextView myTextView1, myTextView2, myTextView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myTextView = (TextView) findViewById(R.id.textView);
+        myTextView1 = (TextView) findViewById(R.id.textView1);
+        myTextView2 = (TextView) findViewById(R.id.textView2);
+        myTextView3 = (TextView) findViewById(R.id.textView3);
 
         test();
     }
@@ -28,12 +30,14 @@ public class MainActivity extends AppCompatActivity {
 
         ChangeColor changeColor = new ChangeColor(this);
 
+        //preferably use ananymous objects , if not because of infinite loops the variable values gets overrided by next method arguments
+        new ChangeColor(this).changeTextColor(myTextView1, "03FBFF", "FF7803");//without delay default is 225ms
+        new ChangeColor(this).changeTextColor(myTextView2, "000000", "FFFFFF", 500);//with delay
+        new ChangeColor(this).changeTextColor(myTextView3, "03FBFF", "FF7803", "FFEA", 500);//with step value
 
-         //changeColor.changeTextColor(myTextView,"03FBFF", "FF7803");//without delay default is 225ms
-        //changeColor.changeTextColor(myTextView, "03FBFF", "FF7803", 500);//with delay
-
-         changeColor.changeBackgroundColor(myTextView, "03FBFF", "FF7803");//without delay default is 225ms
-        //changeColor.changeBackgroundColor(myTextView, "03FBFF", "FF7803", 500);// with delay
+        new ChangeColor(this).changeBackgroundColor(myTextView1, "03ABFA", "FF7803");//without delay default is 225ms
+        new ChangeColor(this).changeBackgroundColor(myTextView2, "03FBAA", "FF0003", 500);//with delay
+        new ChangeColor(this).changeBackgroundColor(myTextView3, "03ABFA", "FF7803", "FFEA", 225);// with step value
 
 
         String s = changeColor.generateTheColorCode();
--- backgroungcolorchange/src/main/java/e/nasirbashak/backgroungcolorchange/ChangeColor.java
+++ backgroungcolorchange/src/main/java/e/nasirbashak/backgroungcolorchange/ChangeColor.java
@@ -10,15 +10,20 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+
 public class ChangeColor extends AppCompatActivity {
 
-    static int startNum = 0, temp;
+    private int startNum = 0, temp;
+    private int stepNum;
 
-    static int endNum = 0;
-    protected Context c;
+    private int endNum = 0;
+    protected Context context;
 
-    public ChangeColor(Context c) {
-        this.c = c;
+    public ChangeColor(Context context) {
+
+        this.context = context;
+        stepNum = Integer.parseInt("FEAA", 16);
     }
 
 
@@ -61,13 +66,58 @@ public class ChangeColor extends AppCompatActivity {
 
     }
 
-
     public void changeBackgroundColor(final View v, String start, String end, final int delay) {
 
 
         startNum = Integer.parseInt(start, 16);
         temp = startNum;
         endNum = Integer.parseInt(end, 16);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+
+                for (; ; ) {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+
+                            v.setBackgroundColor(Color.parseColor(generateTheColorCode()));
+
+                            // Stuff that updates the UI
+
+                        }
+                    });
+
+                    try {
+                        Thread.sleep(delay);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+        }).start();
+
+    }
+
+    public void changeBackgroundColor(final View v, String start, String end, String step, final int delay) {
+
+
+        startNum = Integer.parseInt(start, 16);
+        temp = startNum;
+        endNum = Integer.parseInt(end, 16);
+        stepNum = Integer.parseInt(step, 16);
+
+        if (endNum - startNum < stepNum) {
+            Toast.makeText(context, "step " + step + " should be in multiples of difference in end " + end + " and start " + start, Toast.LENGTH_LONG).show();
+
+            return;
+        }
 
 
         new Thread(new Runnable() {
@@ -127,7 +177,7 @@ public class ChangeColor extends AppCompatActivity {
                                 } else if (v instanceof Button) {
                                     ((Button) v).setTextColor(Color.parseColor(generateTheColorCode()));
                                 } else {
-                                    Toast.makeText(c, "InCompitable View object \n cannot set text color to " + v, Toast.LENGTH_LONG).show();
+                                    Toast.makeText(context, "InCompitable View object \n cannot set text color to " + v, Toast.LENGTH_LONG).show();
                                     Log.d("InCompitable Viewobject", "cannot set text color to " + v);
                                     r[0] = false;
                                 }
@@ -152,7 +202,6 @@ public class ChangeColor extends AppCompatActivity {
         }).start();
 
     }
-
 
     public void changeTextColor(final View v, String start, String end, final int delay) {
 
@@ -179,7 +228,65 @@ public class ChangeColor extends AppCompatActivity {
                                 } else if (v instanceof Button) {
                                     ((Button) v).setTextColor(Color.parseColor(generateTheColorCode()));
                                 } else {
-                                    Toast.makeText(c, "InCompitable View object \n cannot set text color to " + v, Toast.LENGTH_LONG).show();
+                                    Toast.makeText(context, "InCompitable View object \n cannot set text color to " + v, Toast.LENGTH_LONG).show();
+                                    Log.d("InCompitable Viewobject", "cannot set text color to " + v);
+                                    r[0] = false;
+                                }
+
+                            }
+
+
+                            // Stuff that updates the UI
+
+                        }
+                    });
+
+                    try {
+                        Thread.sleep(delay);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+        }).start();
+
+    }
+
+    public void changeTextColor(final View v, String start, String end, String step, final int delay) {
+
+
+        startNum = Integer.parseInt(start, 16);
+        temp = startNum;
+        endNum = Integer.parseInt(end, 16);
+        stepNum = Integer.parseInt(step, 16);
+
+        if (endNum - startNum < stepNum) {
+            Toast.makeText(context, "step " + step + " should be in multiples of difference in end " + end + " and start " + start, Toast.LENGTH_LONG).show();
+
+            return;
+        }
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                final boolean[] r = {true};
+                while (r[0]) {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+
+                            {
+                                if (v instanceof TextView) {
+                                    ((TextView) v).setTextColor(Color.parseColor(generateTheColorCode()));
+                                } else if (v instanceof Button) {
+                                    ((Button) v).setTextColor(Color.parseColor(generateTheColorCode()));
+                                } else {
+                                    Toast.makeText(context, "InCompitable View object \n cannot set text color to " + v, Toast.LENGTH_LONG).show();
                                     Log.d("InCompitable Viewobject", "cannot set text color to " + v);
                                     r[0] = false;
                                 }
@@ -210,10 +317,11 @@ public class ChangeColor extends AppCompatActivity {
 
         String hexString = "";
 
-        if (startNum + Integer.parseInt("FEAA", 16) >= endNum) {
+        if (startNum + stepNum >= endNum) {
             startNum = temp;
         }
-        hexString = Integer.toHexString(startNum += Integer.parseInt("FEAA", 16));
+
+        hexString = Integer.toHexString(startNum += stepNum);
         if (hexString.length() < 6) {
             int size = hexString.length();
 
